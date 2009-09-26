a58a1c1f3a42b57a14614a3e2b890c7cf0d3e02b
Crest <crest@cyb0rg.org>
2009-09-27 00:34:52 +0200

    del

--- main.c
+++ main.c
@@ -8,9 +8,10 @@ int main ( ) {
 	TCCR0B = (1 <<  WGM02) | (1 <<  CS01);
 	OCR0A  = 52 - 4;
 	OCR0B  = OCR0A / 2;
-
+	ACSR   = (1 << ACD);
+	
 	while (1) { 
-	/*	OCR0A += 6;
+		/* OCR0A += 6;
 		OCR0B = OCR0A / 2;
 		_delay_ms( 5 );
 		OCR0A -= 6;
