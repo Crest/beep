c4ff95fa86a13d469940bb1f07baa65d4cd898cf
Crest <crest@cyb0rg.org>
2009-09-27 17:51:55 +0200

    real cleanup

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+void set_freq( uint16_t freq_tone ) {
+	uint32_t freq_cpu = F_CPU;
+	uint32_t clocks   = freq_cpu / freq_tone;
+	
+	if ( clocks > 0xFFFF )
+		return; 
+	
+	TCCR0B &= ~( (1 << CS02) | (1 << CS01) | (1 << CS00) );
+	if ( clocks < 256 ) {
+		TCCR0B |= ( 0 << CS02 ) | ( 0 << CS01 ) | ( 1 CS00 );
+		OCR0A   = clocks;
+	} else if ( clocks < 256 *   8 ) {
+		
+	} else if ( clocks < 256 *  64 ) {
+
+	} else if ( clocks < 256 * 256 ) {
+	
+	}
+	
+	OCR0B = OCR0A / 2;
+}
+
 int main ( ) {
 	DDRB   = (1 << 1);
 	TCCR0A = (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
