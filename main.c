61265ba7ffd533d4e1eab4f8c42f6c02dd78fbb4
Crest <crest@cyb0rg.org>
2009-09-27 16:14:55 +0200

    cleanup

--- main.c
+++ main.c
@@ -6,11 +6,13 @@ int main ( ) {
 	DDRB   = (1 << 1);
 	TCCR0A = (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
 	TCCR0B = (1 <<  WGM02) | (1 <<  CS01);
-	OCR0A  = 52 - 4;
+	OCR0A  = 20; // 52 - 4;
 	OCR0B  = OCR0A / 2;
 	ACSR   = (1 << ACD);
 	
 	while (1) { 
+		OCR0A += 2; OCR0B++;
+		_delay_ms( 1000 );
 		/* OCR0A += 6;
 		OCR0B = OCR0A / 2;
 		_delay_ms( 5 );
